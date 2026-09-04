d5eed7cbb9fd2330f30e2735c7f0e16e59418ca5
n3423324-ui <n3423324@gmail.com>
2026-09-04 17:37:00 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,13 +1,24 @@
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "sqlite:///./nova.db"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./nova.db"
+
+
+# Railway يستخدم postgres:// أحياناً
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
 
 
 APP_NAME = "NOVA AI STUDIO"
