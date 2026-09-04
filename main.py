c2951dd36d6d7e558c32761ba288efdfeeaa3fb3
n3423324-ui <n3423324@gmail.com>
2026-09-04 19:51:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import json
-
-from services.scene_generator import generate_scenes
-from services.image_generator import generate_image_prompts
-
 from fastapi import FastAPI, Request, Depends, Form
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
+import json
+
 from app.database import Base, engine, get_db
 from app.models import Project
+
 from services.script_generator import generate_script
+from services.scene_generator import generate_scenes
+from services.image_generator import generate_image_prompts
+from services.voice_generator import generate_voice
+from services.video_generator import create_video
 
 
 # إنشاء الجداول
@@ -66,6 +68,7 @@ def create_project(
     db: Session = Depends(get_db)
 ):
 
+    # 1- إنشاء القصة
     script = generate_script(
         title,
         idea,
@@ -75,6 +78,7 @@ def create_project(
     )
 
 
+    # 2- إنشاء المشروع
     project = Project(
         title=title,
         idea=idea,
@@ -82,30 +86,68 @@ def create_project(
         language=language,
         duration=duration,
         video_type=video_type,
-        script=script
+        script=script,
+        status="Script Generated"
     )
 
 
     db.add(project)
     db.commit()
     db.refresh(project)
-scenes = generate_scenes(script)
 
-image_prompts = generate_image_prompts(
-    scenes
-)
 
-project.images = json.dumps(
-    image_prompts,
-    ensure_ascii=False
-)
 
-project.status = "Images Ready"
+    # 3- استخراج المشاهد
+    scenes = generate_scenes(
+        script
+    )
 
-db.commit()
+
+    # 4- إنشاء أوصاف الصور
+    image_prompts = generate_image_prompts(
+        scenes
+    )
+
+
+    project.images = json.dumps(
+        image_prompts,
+        ensure_ascii=False
+    )
+
+    project.status = "Images Ready"
+
+
+    # 5- إنشاء الصوت
+    voice = generate_voice(
+        script,
+        language
+    )
+
+
+    project.voice_path = voice
+
+
+
+    # 6- إنشاء الفيديو
+    video = create_video(
+        image_prompts,
+        voice
+    )
+
+
+    project.video_path = video
+
+    project.status = "Video Created"
+
+
+    db.commit()
+    db.refresh(project)
+
+
 
     return {
         "message": "Project created successfully",
         "id": project.id,
+        "status": project.status,
         "script": script
     }
